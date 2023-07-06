faf1b671c55df6ba69787f0ec8b71784993b9fb3
Michela30 <micheladestefano30@gmail.com>
2023-07-06 14:39:00 +0200

    milestone 1 statica

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ createApp({
             contacts: [
                     {
                     name: 'Michele',
-                    avatar: './img/avatar_1.png',
+                    avatar: './img/avatar_1.jpg',
                     visible: true,
                     messages: [
                             {
@@ -36,7 +36,7 @@ createApp({
                     },
                     {
                         name: 'Fabio',
-                        avatar: './img/avatar_2.png',
+                        avatar: './img/avatar_2.jpg',
                         visible: true,
                         messages: [
                             {
@@ -58,7 +58,7 @@ createApp({
                     },
                     {
                         name: 'Samuele',
-                        avatar: './img/avatar_3.png',
+                        avatar: './img/avatar_3.jpg',
                         visible: true,
                         messages: [
                             {
@@ -80,7 +80,7 @@ createApp({
                     },
                     {
                         name: 'Alessandro B.',
-                        avatar: './img/avatar_4.png',
+                        avatar: './img/avatar_4.jpg',
                         visible: true,
                         messages: [
                             {
@@ -97,7 +97,7 @@ createApp({
                     },
                     {
                         name: 'Alessandro L.',
-                        avatar: './img/avatar_5.png',
+                        avatar: './img/avatar_5.jpg',
                         visible: true,
                         messages: [
                             {
@@ -114,7 +114,7 @@ createApp({
                     },
                     {
                         name: 'Claudia',
-                        avatar: './img/avatar_5.png',
+                        avatar: './img/avatar_5.jpg',
                         visible: true,
                         messages: [
                             {
@@ -136,7 +136,7 @@ createApp({
                     },
                     {
                         name: 'Federico',
-                        avatar: './img/avatar_7.png',
+                        avatar: './img/avatar_7.jpg',
                         visible: true,
                         messages: [
                             {
@@ -153,7 +153,7 @@ createApp({
                     },
                     {
                         name: 'Davide',
-                        avatar: './img/avatar_8.png',
+                        avatar: './img/avatar_8.jpg',
                         visible: true,
                         messages: [
                             {
