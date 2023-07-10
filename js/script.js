2e9dd12322dce1c8fbc9541ddd836817b2f5a10a
Michela30 <micheladestefano30@gmail.com>
2023-07-10 16:34:33 +0200

    add varianti

--- js/script.js
+++ js/script.js
@@ -31,12 +31,21 @@ createApp({
             newMessage: '',
             sayOk: 'Ok',
             searchName: '',
+            contactsColors: {
+                'Mamma': 'text-warning',
+                'Papà': 'text-info',
+                'Valerio':'text-danger',
+                'Io':'text-success',
+                'Ramona':'text-primary',
+                'Isabella':'text-danger-emphasis'
+            },
             contacts: [
                     {
                     name: 'Michele',
                     avatar: './img/avatar_1.jpg',
                     visible: true,
                     isActive: true,
+                    silent: false,
                     messages: [
                             {
                             date: '10/01/2020 15:30:55',
@@ -60,6 +69,7 @@ createApp({
                         avatar: './img/avatar_2.jpg',
                         visible: true,
                         isActive: false,
+                        silent: false,
                         messages: [
                             {
                             date: '20/03/2020 16:30:00',
@@ -83,6 +93,7 @@ createApp({
                         avatar: './img/avatar_3.jpg',
                         visible: true,
                         isActive: false,
+                        silent: false,
                         messages: [
                             {
                             date: '28/03/2020 10:10:40',
@@ -106,6 +117,7 @@ createApp({
                         avatar: './img/avatar_4.jpg',
                         visible: true,
                         isActive: false,
+                        silent: false,
                         messages: [
                             {
                             date: '10/01/2020 15:30:55',
@@ -120,19 +132,20 @@ createApp({
                         ],
                     },
                     {
-                        name: 'Alessandro L.',
+                        name: 'Valerio',
                         avatar: './img/avatar_5.jpg',
                         visible: true,
                         isActive: false,
+                        silent: false,
                         messages: [
                             {
                             date: '10/01/2020 15:30:55',
-                            message: 'Ricordati di chiamare la nonna',
+                            message: 'Ricordati di chiamare nonno',
                             status: 'sent'
                             },
                             {
                             date: '10/01/2020 15:50:00',
-                            message: 'Va bene, stasera la sento',
+                            message: 'Va bene, stasera lo sento',
                             status: 'received'
                             }
                         ],
@@ -142,6 +155,7 @@ createApp({
                         avatar: './img/avatar_6.jpg',
                         visible: true,
                         isActive: false,
+                        silent: false,
                         messages: [
                             {
                             date: '10/01/2020 15:30:55',
@@ -165,6 +179,7 @@ createApp({
                         avatar: './img/avatar_7.jpg',
                         visible: true,
                         isActive: false,
+                        silent: false,
                         messages: [
                             {
                             date: '10/01/2020 15:30:55',
@@ -179,10 +194,44 @@ createApp({
                         ],
                     },
                     {
+                        name: 'Tozzetto\'s Family',
+                        avatar: 'https://cdn-icons-png.flaticon.com/512/7051/7051035.png',
+                        visible: true,
+                        isActive: false,
+                        silent: true,
+                        messages: [
+                            {
+                            nameFamily: 'Mamma',
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao ragazzi, non mi funziona internet come posso fare?',
+                            status: 'received'
+                            },
+                            {
+                            nameFamily: 'Papà',
+                            date: '10/01/2020 15:50:00',
+                            message: 'Michela stasera mi guardi il telefono? C\'è un messaggio strano',
+                            status: 'received'
+                            },
+                            {
+                            nameFamily: 'Valerio',
+                            date: '10/01/2020 15:51:00',
+                            message: 'Mamma stacca la spina e riavvia il modem!',
+                            status: 'received'
+                            },
+                            {
+                            nameFamily: 'Io',
+                            date: '10/01/2020 15:51:00',
+                            message: 'Ok stasera lo vediamo.. che pazienza!',
+                            status: 'sent'
+                            }
+                        ],
+                    },
+                    {
                         name: 'Davide',
                         avatar: './img/avatar_8.jpg',
                         visible: true,
                         isActive: false,
+                        silent: false,
                         messages: [
                             {
                             date: '10/01/2020 15:30:55',
@@ -200,14 +249,49 @@ createApp({
                             status: 'received'
                             }
                         ],
-                    }
-                ]
+                    },
+                    {
+                        name: 'Girls\' Power',
+                        avatar: 'https://us.123rf.com/450wm/yupiramos/yupiramos1902/yupiramos190204518/125388778-group-of-women-avatars-characters-vector-illustration-design.jpg?ver=6',
+                        visible: true,
+                        isActive: false,
+                        silent: false,
+                        messages: [
+                            {
+                            nameFamily: 'Ramona',
+                            date: '10/01/2020 15:30:55',
+                            message: 'Stasera che facciamo? Andiamo in palestra?',
+                            status: 'received'
+                            },
+                            {
+                            nameFamily: 'Isabella',
+                            date: '10/01/2020 15:50:00',
+                            message: 'Io ho il turno domani mattina, però ci sono!',
+                            status: 'received'
+                            },
+                            {
+                            nameFamily: 'Io',
+                            date: '10/01/2020 15:51:00',
+                            message: 'Ovvio, io vado a Pilates alle 19, ci vediamo lì?',
+                            status: 'sent'
+                            },
+                            {
+                            nameFamily: 'Ramona',
+                            date: '10/01/2020 15:51:00',
+                            message: 'Uffa, a me Pilates non piaceee',
+                            status: 'received'
+                            }
+                        ],
+                    },
+                ],
+            mioAvatar: 'https://static.vecteezy.com/ti/vettori-gratis/p1/24183541-femmina-avatar-bionda-donna-ritratto-vettore-illustrazione-di-un-femmina-personaggio-nel-un-moderno-colore-stile-vettoriale.jpg' 
             }
         },
         methods: {
             switchChat(currentlyActiveIndex){
             
-                this.contacts[this.currentlyActiveIndex].isActive = false;
+                this.contacts[this.currentlyActiveIndex].isActive = false
+                silent: false;
                 
                 this.currentlyActiveIndex = currentlyActiveIndex;
 
@@ -246,7 +330,7 @@ createApp({
             },
             removeMessage(index){
                 this.contacts[this.currentlyActiveIndex].messages.splice(index,1)
-            }
+            },
         }
     }).mount('#app');
     
